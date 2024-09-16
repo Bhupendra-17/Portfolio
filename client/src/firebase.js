42f7e4771eb23a7b93280bcb7d1c10637366b439
Bhupendra-17 <bhupendrad1724@gmail.com>
2024-09-16 14:51:19 +0530

    tried to deploy

---
+++ client/src/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBOeip9MkUWPFC3GgzwVM7UPy6PnIIjUD4",
+  authDomain: "portfolio-bhupendra.firebaseapp.com",
+  projectId: "portfolio-bhupendra",
+  storageBucket: "portfolio-bhupendra.appspot.com",
+  messagingSenderId: "188336050076",
+  appId: "1:188336050076:web:fcdd169cccd7ad495fc259"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
