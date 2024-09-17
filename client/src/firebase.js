797e47d7d6fb0d7398bfcf9433afa25c94d4adfe
Bhupendra-17 <bhupendrad1724@gmail.com>
2024-09-17 12:51:33 +0530

    Changes made in resume

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,14 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBOeip9MkUWPFC3GgzwVM7UPy6PnIIjUD4",
-  authDomain: "portfolio-bhupendra.firebaseapp.com",
-  projectId: "portfolio-bhupendra",
-  storageBucket: "portfolio-bhupendra.appspot.com",
-  messagingSenderId: "188336050076",
-  appId: "1:188336050076:web:fcdd169cccd7ad495fc259"
+  apiKey: "AIzaSyB3YwSX8JCt4zxrMgVoOOMq6jSsqTksAp4",
+  authDomain: "bhupendra-dewangan-17.firebaseapp.com",
+  projectId: "bhupendra-dewangan-17",
+  storageBucket: "bhupendra-dewangan-17.appspot.com",
+  messagingSenderId: "430215075940",
+  appId: "1:430215075940:web:9957aa2524263ebf0a2771"
 };
 
 // Initialize Firebase
